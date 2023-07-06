9d3f328bf029164b15c5bb96c9e872487cfe3a59
unknownbox <ekdmsdlfma@gmail.com>
2023-07-06 19:59:46 +0900

    modified:   simulation.py

--- simulation.py
+++ simulation.py
@@ -7,111 +7,37 @@ WIDTH = 50
 HEIGHT = 50
 HORIZONTAL = 0
 VERTICAL = 1
-DT = decimal.Decimal('0.01')
-ALPHA = decimal.Decimal('0.5')
+DT = 0.01
+ALPHA = 0.1
+ALPHA /= 4
 
 def posToVector(p1, p2, forceValue) -> tuple:
-    '''
-    위치를 벡터(튜플)로 바꿔준다. 마우스를 클릭했을 때 어느정도의 힘을 줄 지 계산할 수 있다.
-    '''
     theta = math.atan2(p1[1] - p2[1], p1[0] - p2[0])
     return (forceValue * math.cos(theta), forceValue * math.sin(theta))
 
 class VectorField:
-    '''
-    벡터장이다. 라플라스 방정식의 원리를 이용해서 각 점에서의 힘을 계산할 수 있다.
-    '''
     def __init__(self, width = WIDTH, height = HEIGHT) -> None:
-        '''
-        VectorField(width = WIDTH, height = HEIGHT)
-        기본 값으로 WIDTH와 HEIGHT를 제공한다. 가로 width, 세로 height 크기의 벡터장을 만든다.
-        '''
         self.width = width
         self.height = height
-        self.field = [[(decimal.Decimal(0), decimal.Decimal(0))] * width for _ in range(height)]
-        self.visited = [[False] * width for _ in range(height)]
+        self.field = np.zeros((width, height, 2))
     
     def addForce(self, pos, force, team = None) -> None:
-        '''
-        addForce(pos, force) -> None
-        pos는 1개 이상의 좌표들이 담긴 리스트로 주어져야 한다. 
-        force는 1개 이상의 벡터들이 담긴 리스트로 주어져야 한다.
-        '''
-        startPoints = deque()
         for i in range(len(pos)):
-            np = self.field[pos[i][0]][pos[i][1]]
-            self.field[pos[i][0]][pos[i][1]] = (np[0] + force[i][0], np[1] + force[i][1])
-            #self.visited[pos[i][0]][pos[i][1]] = True
-            startPoints.append(pos[i])
-        self.getPointForce(startPoints)
+            nv = self.field[pos[i][1], pos[i][0]]
+            self.field[pos[i][1], pos[i][0]] = (nv[1] + force[i][0], nv[0] + force[i][1])
+        self.getPointForce()
 
-    def getPointForce(self, startPoints: deque, alpha = ALPHA, dt = DT) -> None:
-        '''
-        BFS를 이용해서 각 점에서의 힘을 구할 수 있다.
-        '''
-        '''
-        queue = deque()
-        while startPoints:
-            y, x = startPoints.popleft()
-            if x != 0:
-                if not self.visited[y][x - 1]:
-                    self.visited[y][x - 1] = True
-                    queue.append((y, x - 1))
-            if x != self.width- 1:
-                if not self.visited[y][x + 1]:
-                    self.visited[y][x + 1] = True
-                    queue.append((y, x + 1))
-            if y != 0:
-                if not self.visited[y - 1][x]:
-                    self.visited[y - 1][x] = True
-                    queue.append((y - 1, x))
-            if y != self.height - 1:
-                if not self.visited[y + 1][x]:
-                    self.visited[y + 1][x] = True
-                    queue.append((y + 1, x))
-        '''
-        def df(y, x, mod = HORIZONTAL):
-            if mod == HORIZONTAL:
-                if x < self.width:
-                    right = self.field[y][x + 1][0] if x < self.width - 1 else 0
-                    left = self.field[y][x - 1][0] if x > 0 else 0
-                    return (right - left)
-            else:
-                if y < self.height:
-                    right = self.field[y + 1][x][1] if y < self.height - 1 else 0
-                    left = self.field[y - 1][x][1] if y > 0 else 0
-                    return (right - left)
-            return 0
-            
-        queue = copy.copy(startPoints)
+    def getPointForce(self, alpha = ALPHA, dt = DT) -> None:
+        newField = np.zeros((self.width, self.height, 2))
         for _ in range(int(1/dt)):
-            while queue:
-                y, x = queue.popleft()
-                d2v = [0] * 2
-                if x != 0:
-                    if not self.visited[y][x - 1]:
-                        self.visited[y][x - 1] = True
-                        queue.append((y, x - 1))
-                if x != self.width - 1:
-                    if not self.visited[y][x + 1]:
-                        self.visited[y][x + 1] = True
-                        queue.append((y, x + 1))
-                if y != 0:
-                    if not self.visited[y - 1][x]:
-                        self.visited[y - 1][x] = True
-                        queue.append((y - 1, x))
-                if y != self.height - 1:
-                    if not self.visited[y + 1][x]:
-                        self.visited[y + 1][x] = True
-                        queue.append((y + 1, x))
-                d2v[0] = (df(y, x + 1, HORIZONTAL) - df(y, x - 1, HORIZONTAL)) / 2
-                d2v[1] = (df(y + 1, x, VERTICAL) - df(y - 1, x, VERTICAL)) / 2
-                if (y, x) == (HEIGHT//2-1, WIDTH//2-1) : print(d2v)
-                self.field[y][x] = (self.field[y][x][0] + d2v[0] * alpha, self.field[y][x][1] + d2v[1] * alpha)
-            self.visited = [[False] * self.width for _ in range(self.height)]
-            queue = copy.copy(startPoints)
-            #for i in range(HEIGHT) : plt.plot(list(map(lambda x : ((math.sqrt(x[0]*x[0] + x[1]*x[1]))), self.field[i])))
-            #plt.show()
+            for x in range(1, self.width - 1):
+                for y in range(1, self.height - 1):
+                    newField[x, y, 0] = alpha * (self.field[x + 1, y, 0] + self.field[x - 1, y, 0] + self.field[x, y + 1, 0] + self.field[x, y - 1, 0] - 4 * self.field[x, y, 0]) + self.field[x, y, 0]
+                    newField[x, y, 1] = alpha * (self.field[x + 1, y, 1] + self.field[x - 1, y, 1] + self.field[x, y + 1, 1] + self.field[x, y - 1, 1] - 4 * self.field[x, y, 1]) + self.field[x, y, 1]
+            self.field = newField[:, :, :]
+            if _%10 == 0:
+                for i in range(HEIGHT) : plt.plot(list(map(lambda x : ((math.sqrt(x[0]*x[0] + x[1]*x[1]))), self.field[i])))
+                plt.show()
 
 class GameField:
     def __init__(self, width = WIDTH, height = HEIGHT) -> None:
@@ -131,9 +57,10 @@ class GameField:
                         if self.height > y + i * n[0] > 0 and self.width > x + i * n[1] > 0:
                             self.team[y + i * n[0]][x + i * n[1]] = 1
 
-sum_ = lambda x : x*(x+1)//2
 gf = GameField()
-gf.force.addForce([(HEIGHT//2-1, WIDTH//2-1)],[(-30,-30)])
+midWidth = WIDTH//2-1
+midHeight = HEIGHT//2-1
+gf.force.addForce([(midWidth, midHeight)],[(100,10)])
 gf.update()
 
 data = gf.force.field
@@ -142,7 +69,6 @@ plt.imshow([list(map(lambda x : ((math.sqrt(x[0]*x[0] + x[1]*x[1]))), data[i])) 
 plt.show()
 plt.figure(figsize=(WIDTH,HEIGHT))
 plt.imshow(gf.team)
-print(gf.team)
 plt.show()
 for i in range(HEIGHT) : plt.plot(list(map(lambda x : ((math.sqrt(x[0]*x[0] + x[1]*x[1]))), data[i])))
 plt.show()
